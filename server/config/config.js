4d2971d2df3bc37702ef8134fe4331e0536354e6
sonivaibhav <sonivaibhav332@gmail.com>
2015-02-18 20:31:04 -0800

    changed mongolab to compose in mongoose.js file

--- server/config/config.js
+++ server/config/config.js
@@ -2,13 +2,15 @@ var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
     development: {
-        db: 'mongodb://localhost/feg-db',
+//        db: 'mongodb://localhost/feg-db',
+        db: 'mongodb://vaibhav:vaibhav007@kahana.mongohq.com:10077/meanjs',
         rootPath: rootPath,
         port: process.env.PORT || 3030
     },
     production: {
         rootPath: rootPath,
-        db:'mongodb://vsoni:frontendguide@ds043991.mongolab.com:43991/front-end-guide',
-        port: process.env.PORT || 80  
+//        db:'mongodb://vsoni:frontendguide@ds043991.mongolab.com:43991/front-end-guide',
+        db:'mongodb://vaibhav:vaibhav007@kahana.mongohq.com:10077/meanjs',
+        port: process.env.PORT || 80
     }
 }
