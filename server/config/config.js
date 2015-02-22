60120f6eed8065fd71a0742a57c1f0e022ee81f8
sonivaibhav <sonivaibhav332@gmail.com>
2015-02-22 13:10:29 -0500

    Implemented Sign up functionality

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
     development: {
-//        db: 'mongodb://localhost/feg-db',
-        db: 'mongodb://vaibhav:vaibhav007@kahana.mongohq.com:10077/meanjs',
+        db: 'mongodb://localhost/feg-db',
+//        db: 'mongodb://vaibhav:vaibhav007@kahana.mongohq.com:10077/meanjs',
         rootPath: rootPath,
         port: process.env.PORT || 3030
     },
